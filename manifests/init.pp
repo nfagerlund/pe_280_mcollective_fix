b6635015df4a48bd41100fdd80546069d1e3a152
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-04-05 14:52:27 -0700

    No need to inherit -- include and assign instead
    I like this because it's more explicit and has fewer side effects. Inheriting
    is good if we're using class params and want the ability to set defaults, but
    we aren't and don't.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,11 @@
-class pe_280_mcollective_fix inherits pe_280_mcollective_fix::params {
+class pe_280_mcollective_fix {
+  # Only do things on PE 2.8.0 systems with MCollective.
   if str2bool("${::is_pe}") and $::pe_version == '2.8.0' and $::osfamily != 'windows' {
+
+    include pe_280_mcollective_fix::params
+    $stomp_source   = $pe_280_mcollective_fix::params::stomp_source
+    $stomp_name     = $pe_280_mcollective_fix::params::stomp_name
+    $stomp_provider = $pe_280_mcollective_fix::params::stomp_provider
 
     if $stomp_source =~ /FAIL/ {
       fail("${module_name}: Something went wrong, and we couldn't find a package appropriate for this system.")
@@ -31,7 +37,7 @@ class pe_280_mcollective_fix inherits pe_280_mcollective_fix::params {
       ],
     }
   } else {
-    notice("Nothing for ${module_name} to do on this system -- either no PE, no MCollective, or not broken.") 
+    notice("Nothing for ${module_name} to do on this system -- either no PE, no MCollective, or not broken.")
   }
 }
 
