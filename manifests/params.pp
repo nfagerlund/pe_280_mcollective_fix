f94e3ba7245c32db4be78c27aa9fccd2a3c970d6
Nick Fagerlund <nick.fagerlund@gmail.com>
2013-04-05 14:56:48 -0700

    Forget about the stomp doc package on redhat/sles
    Per Moses, we don't need this, and we probably don't even install it in the PE
    installer, we just provide it.

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,22 +13,10 @@ class pe_280_mcollective_fix::params {
         /^5/: {
           $stomp_name   = 'pe-rubygem-stomp-1.2.3-1.1.9.pe.el5.noarch.rpm'
           $stomp_source = "${source_base}/el/${stomp_name}"
-
-          package { 'pe-rubygem-stomp-doc-1.2.3-1.1.9.pe.el5.noarch.rpm':
-            ensure   => installed,
-            provider => rpm,
-            source   => "${source_base}/el/pe-rubygem-stomp-doc-1.2.3-1.1.9.pe.el6.noarch.rpm",
-          }
         }
         /^6/: {
           $stomp_name   = 'pe-rubygem-stomp-1.2.3-1.1.9.pe.el6.noarch.rpm'
           $stomp_source = "${source_base}/el/${stomp_name}"
-
-          package { 'pe-rubygem-stomp-doc-1.2.3-1.1.9.pe.el6.noarch.rpm':
-            ensure   => installed,
-            provider => rpm,
-            source   => "${source_base}/el/pe-rubygem-stomp-doc-1.2.3-1.1.9.pe.el6.noarch.rpm",
-          }
         }
         default: {
           $stomp_source = 'FAIL'
@@ -39,12 +27,6 @@ class pe_280_mcollective_fix::params {
       $stomp_provider = 'rpm'
       $stomp_name     = 'pe-rubygem-stomp-1.2.3-1.1.9.pe.noarch.rpm'
       $stomp_source   = "${source_base}/sles/${stomp_name}"
-
-      package { 'pe-rubygem-stomp-doc-1.2.3-1.1.9.pe.noarch.rpm':
-        ensure   => installed,
-        provider => rpm,
-        source   => "${source_base}/suse/pe-rubygem-stomp-doc-1.2.3-1.1.9.pe.noarch.rpm",
-      }
     }
     'debian': {
       $stomp_provider = 'dpkg'
